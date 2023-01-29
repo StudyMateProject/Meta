723f4d9b0a1b4b85710db5233dcd393f77f3de56
PSungChul <sksh0000@naver.com>
2023-01-30 02:34:12 +0900

    주석 수정

--- src/main/java/com/study/soju/entity/Meta.java
+++ src/main/java/com/study/soju/entity/Meta.java
@@ -139,8 +139,8 @@ public class Meta {
 
         // 모집인원이 정원초과일 경우 에러메세지를 DTO로 변환 (생성자 방식)
         public rpEntrance(String err) { // 3. 파라미터로 서비스에서 넘어온 에러메세지를 받아온다.
-            this.metaIdx = 0; // metaIdx는 0으로 고정
-            this.metaTitle = err; // metaTitle은 받아온 에러메세지를 저장
+            this.metaIdx = 0; // metaIdx는 0으로 고정해서 에러 체크값으로 사용한다.
+            this.metaTitle = err; // metaTitle은 받아온 에러메세지를 저장한다.
         }
     }
 }
